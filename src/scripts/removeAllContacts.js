dc096634e852ec723cca2f0ed928b0cbf7a775a9
VolodymyrVasylyna <143510572+VolodymyrVasylyna@users.noreply.github.com>
2024-07-03 18:14:48 +0300

    sum

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,17 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    if (fs.existsSync(PATH_DB)) {
+      fs.writeFileSync(PATH_DB, '[]');
+      console.log(`All contacts removed from ${PATH_DB}`);
+    } else {
+      console.log(`No contacts found in ${PATH_DB}`);
+    }
+  } catch (error) {
+    console.error('Error removing contacts:', error);
+  }
+};
 
-removeAllContacts();
+await removeAllContacts();
